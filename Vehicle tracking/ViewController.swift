b70e4c4815b84ad7493a260801d64e86c7564049
Artyom Jalilov <ajalilov@scnsoft.com>
2022-12-21 18:21:06 +0100

    Initial Commit

---
+++ Vehicle tracking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vehicle tracking
+//
+//  Created by Jalilov, Artyom on 21.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
